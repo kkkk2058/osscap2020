55749340a319f02cc1a51bb165070242549f6bd6
danielseo77 <72370353+danielseo77@users.noreply.github.com>
2020-12-02 20:20:21 +0900

    마스크게임 일단 한데까지 올림(미완성)

---
+++ maskgame_임시용.py
@@ -0,0 +1,131 @@
+from matrix 
+import time
+import pygame
+import sys
+import threading
+import random
+
+def LED_init():
+    thread=threading.Thread(target=LMD.main, args=())
+    thread.setDaemon(True)
+    thread.start()
+    return
+
+def draw_matrix(m):
+    
+    array = m.get_array()  # matrix의 객체로 부터 array라는 리스트 생
+    for y in range(m.get_dy()-4):  # y는 matrix의 각 행을 말함.
+        for x in range(4, m.get_dx()-4):
+            if array[y][x] == 0:
+                LMD.set_pixel(x, 19-y, 0)
+            elif array[y][x] == 1:
+                LMD.set_pixel(x, 19-y, 1)
+            elif array[y][x] == 2:
+                LMD.set_pixel(x, 19-y, 2)
+        print()
+
+arrayScreen = [
+[7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+[7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+[7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7], 
+[7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],  # 0
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],  # 2
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],  # 4
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],  # 6
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],  # 8
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],  # 10
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],  # 12
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],  # 14
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 7, 7, 7, 7, 7],
+[7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+[7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+[7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7], 
+[7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7],
+]        
+
+def mask():
+    #초기화 #중요!
+    pygame.init() 
+
+    score = 0
+    #FPS
+    clock = pygame.time.Clock()
+
+    #화면 크기 설정
+    screenWidth = 480 #가로크기
+    screenHeight = 640 #세로크기
+
+    def block():
+        arrayBlk=[[1,1,1]]
+
+    def char():
+        arrayBlk = [[4, 4], [4, 4]]  # character
+
+    
+    iscreenDy= 16
+    iscreenDx= 32
+
+    characterYpos = 6  # 나오는 도형의 좌측상단의 좌표y=0
+    characterXpos = 28  # 똥이 나오는 x좌표
+
+    enemyYpos = random.randrange(4, 19) # mask 초기 y값
+    enemyXpos = 4
+
+    birusYpos = random.randrange(4, 19) # birus 초기 y값
+    birusXpos = 0
+
+    
+    enemyBlk = Matrix(block())
+
+    birusBlk = Matrix(block())
+
+    poSpeed = 1
+    
+    #LED_init()
+
+    time.time()
+
+    
+    while True:
+        iScreen=Matrix(arrayScreen)
+        oScreen=Matrix(iScreen)
+
+        charBlk = Matrix(char())
+
+        chartempBlk = iScreen.clip(characterYpos, characterXpos, characterYpos + charBlk.get_dy(), characterXpos + charBlk.get_dx())
+        chartempBlk = chartempBlk + charBlk
+        oScreen.paste(chartempBlk, characterYpos, characterXpos)
+        
+        enemytempBlk = iScreen.clip(enemyYpos, enemyXpos, enemyYpos + enemyBlk.get_dy(), enemyXpos + enemyBlk.get_dx())
+        enemytempBlk = enemytempBlk + enemyBlk
+        oScreen.paste(enemytempBlk, enemyYpos, enemyXpos)
+        
+        birustempBlk = iScreen.clip(birusYpos, birusXpos, birusYpos + itemBlk.get_dy(), birusXpos + itemBlk.get_dx())
+        birustempBlk = birustempBlk + itemBlk
+        oScreen.paste(birustempBlk, birusYpos, birusXpos)
+        draw_matrix(oScreen);print()
+
+        enemyXpos += poSpeed
+        birusXpos += poSpeed
+
+        for event in pygame.event.get(): #어떤 이벤트 발생했는지 판단함
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    toX -= characterSpeed
+                if event.key == pygame.K_RIGHT:
+                    toX += characterSpeed
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                    toX = 0
+                elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    toY = 0 
